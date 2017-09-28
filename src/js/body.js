268dede5d8a85e045adb10bf9f776d27918ccf4c
songpeilann <songpeilan@hefantv.com>
2017-09-28 18:59:26 +0800

    update ss

---
+++ src/js/body.js
@@ -0,0 +1,19 @@
+
+import "../css/css.css";
+import "../css/index.less";
+import "./user/user.js";
+
+class A{
+    constructor(){
+
+    }
+    a(){
+        console.log("我是a")
+    }
+    b(){
+        console.log("我是b")
+    }
+}
+
+var obj= new A();
+obj.a();
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
+var postcss = require('postcss-loader');
+let autoprefixer = require('autoprefixer');
 
 
 module.exports = {
@@ -13,8 +15,8 @@ module.exports = {
      * entry选项用来配置要打包的入口文件
      * **/
     entry: {
-        bodyIndex:'./src/index.js',
-        headIndex:'./src/user2.js',
+        bodyIndex:'./src/js/body.js',
+        headIndex:'./src/js/header.js',
     },
 
     /**
@@ -22,7 +24,7 @@ module.exports = {
      * **/
     output: {
         path: path.resolve(__dirname, './build'),
-        filename: 'js/[name]-[hash].js',
+        filename: 'js/[name].js',
         publicPath:'',
     },
 
@@ -41,20 +43,46 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader','css-loader')
+                test: /\.(css|less)$/,
+                use:[
+                    'style-loader',
+                    {loader:'css-loader',options:{importLoaders:1}},
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return [
+                                    require('postcss-import')(),        //一定要写在require("autoprefixer")前面，否则require("autoprefixer")无效
+                                    require("autoprefixer")({browsers:['last 5 versions']})
+                                ]
+                            }
+                        }
+                    }
+                ],
+            },
+            {
+                test:/.(png|jpg)$/,
+                loader:"url-loader?limit=50",
+            },
+            {
+                test:/.(png|jpg)$/,
+                loader:"file-loader",
+                query: {
+                    name: 'img/[name].[ext]'
+                }
             }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css'),
+]
 
+    },
+
+    plugins: [
+        //new ExtractTextPlugin('styles.css'),
         /**
          * 自动将js打到html中
          * **/
         new htmlWebpackPlugin({
             filename:'index.html', //文件名字:可加hash值 index-[hash].html
-            template:'index.html', //文件模板
+            template:'src/index.html', //文件模板
             inject:false,//head 生成到头部 FALSE,不执行
             title:'webpack is good', //参数
             minify:{
